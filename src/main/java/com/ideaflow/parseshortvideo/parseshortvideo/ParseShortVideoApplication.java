32e21da43cc410f1168fe68264a499d15bb94aa4
biliww <843145936@qq.com>
2025-12-30 11:50:50 +0800

    初始化

---
+++ src/main/java/com/ideaflow/parseshortvideo/parseshortvideo/ParseShortVideoApplication.java
@@ -0,0 +1,13 @@
+package com.ideaflow.parseshortvideo.parseshortvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseShortVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParseShortVideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ideaflow/parseshortvideo/parseshortvideo/ParseShortVideoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ideaflow.parseshortvideo.parseshortvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseShortVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
